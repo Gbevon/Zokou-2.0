fd1d1ff119565292eb3887912420b3637b2efa52
LOA <118371674+Gbevon@users.noreply.github.com>
2024-03-10 00:35:35 +0000

    Create Mode_aventures.js

---
+++ commandes/Mode_aventures.js
@@ -0,0 +1,44 @@
+const { zokou } = require('../framework/zokou');
+
+zokou(
+    {
+        nomCom: 'ma',
+        categorie: 'world of hiden blue'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/5c62aeeacdfff30e3ae7b.jpg';
+            const msg = `⭕️ MODE AVENTURES🗺️
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+𖨠🌎 RÉGION :
+𖨠🚸 SUPERFICIE : 280km²
+
+𓏒𓏒𓏒𓏒 NORD🌎𓏒𓏒𓏒𓏒
+
+
+
+
+
+𓏒𓏒𓏒𓏒🌎SUD🌎𓏒𓏒𓏒𓏒
+
+
+
+
+
+
+𓏒𓏒𓏒𓏒🌎 EST🌎𓏒𓏒𓏒𓏒
+
+
+
+
+
+𓏒𓏒𓏒𓏒OUEST🌎𓏒𓏒𓏒𓏒
+⚠️ Cette fiche résume ce qui se passe dans les différents endroits ci-dessus, les joueurs devront écrire leurs panel en fonction de se qui se passe dans l'endroit où ils sont`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
