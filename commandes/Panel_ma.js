a384a4bc0bd60f54302e1cb8e52117fedcedfea7
LOA <118371674+Gbevon@users.noreply.github.com>
2024-03-10 10:44:10 +0000

    Create Panel_ma.js

---
+++ commandes/Panel_ma.js
@@ -0,0 +1,57 @@
+const { zokou } = require('../framework/zokou');
+
+zokou(
+    {
+        nomCom: 'panela',
+        categorie: 'world of hiden blue'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg } = commandeOptions;
+
+        if (!arg[0]) {
+            repondre(`➖➖🛡️PANEL MODE AVENTURE 🛡️➖
+  
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+🌎 RÉGION :
+⚠️ ZONE :
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+🚹LEVEL :1
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+          🧰 RESSOURCES 🧰
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+
+🏇 TRANSPORT :
+🪚 OUTILS TRAVAIL :
+🪙 FINANCE :
+🪵 MATÉRIAUX :
+⚔️ ARMES:
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+👤 players :
+
+           📊 STATISTIQUES 
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+❤️ PV :200%
+🔋power:500%
+🛡️ Défense :50%
+🌀Mana :200%
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+💬chat🎧: 
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+                ÉTAPE 1⤵️
+🔰
+
+                ÉTAPE 2⤵️
+🔰
+𓏒𓏒INVENTAIRE TROUVÉ🧰𓏒𓏒
+🔰
+
+🔰
+🔰
+
+🔰
+🔰
+𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒𓏒
+🌐WORLD OF HIDEN BLUE 🌐`);
+        }
+    }
+);
