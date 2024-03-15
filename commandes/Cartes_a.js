75dd5526626f63354f5648b04827c6a4f81bc4ee
LOA <118371674+Gbevon@users.noreply.github.com>
2024-03-15 19:47:25 +0000

    Update Cartes_a.js

--- commandes/Cartes_a.js
+++ commandes/Cartes_a.js
@@ -17,13 +17,14 @@ zokou(
 𖨠🚸 SUPERFICIE : 280km²
 
              𖨠 DESCRIPTION 𖨠
-GRAFIELD est une région prospères avec un climat et des saisons varié (....) sa sa forêt abrite une grande variété d'espèces animales et végétales ainsi que des surfaces différents par endroits sans oublier sa grande ville qui est un carrefour commercial incontournable et un lieu de découverte historique majeur
+ Bienvenue au point de départ à l'entré de la région de Grafield. 
+ GRAFIELD est une région prospères avec un climat et des saisons varié (....) sa sa forêt abrite une grande variété d'espèces animales et végétales ainsi que des surfaces différents par endroits sans oublier sa grande ville qui est un carrefour commercial incontournable et un lieu de découverte historique majeur
 
 ☢️LOCALISATION☢️:
 > VOICI LES DIFFÉRENTS ZONES AINSI QUE STRUCTURE EN ALLANT AU ⬇️
 
 𓏒𓏒𓏒𓏒NORD🌎𓏒𓏒𓏒𓏒
-Vous verrez une vaste forêt (🌳🪵)s'étendant sur 30km²⚠️ avec un grand cours d'eau  de 5m de largeur au centre 🚣‍♂️de cette forêt la coupant en deux et qui vas jusqu'au chute d'eau (....) les animaux 🐗de cette forêt aiment aller vers ce cours d'eau pour boire 
+a 15km du point de départ vous verrez une vaste forêt (🌳🪵)s'étendant sur 30km²⚠️ avec un grand cours d'eau  de 5m de largeur au centre 🚣‍♂️de cette forêt la coupant en deux et qui vas jusqu'au chute d'eau (....) les animaux 🐗de cette forêt aiment aller vers ce cours d'eau pour boire 
 
 ⚠️🐗『ANIMAUX』🐒⚠️↴
 ➖🫎cerfs et autres herbivores {buffle, antilope,..etc} (430)
@@ -33,13 +34,13 @@ Vous verrez une vaste forêt (🌳🪵)s'étendant sur 30km²⚠️ avec un gran
 ➖ 🐺prédateurs en tout genre {ours,loup,..etc} (200)
 
 𓏒𓏒𓏒𓏒SUD🌎𓏒𓏒𓏒𓏒
-vous verrez une vaste plaine s'étendant sur 35 km² donc vous devez avoir nécessairement des moyens de transport🏇 pour vous faciliter les déplacements sur longue distance (....) cette vaste plaine recouverte de pierre,de roche..etc est parfait pour construire votre demeure 🏰ou un campement ⛺a condition d'avoir des ressources...il peut arriver de croiser par moment d'autres joueurs 👤...et les animaux qui sont dans cette zone sont essentiellement des herbivores 🫎
+a 23km du point de départ vous verrez une vaste plaine s'étendant sur 35 km² donc vous devez avoir nécessairement des moyens de transport🏇 pour vous faciliter les déplacements sur longue distance (....) cette vaste plaine recouverte de pierre,de roche..etc est parfait pour construire votre demeure 🏰ou un campement ⛺a condition d'avoir des ressources...il peut arriver de croiser par moment d'autres joueurs 👤...et les animaux qui sont dans cette zone sont essentiellement des herbivores 🫎
 
 𓏒𓏒𓏒𓏒 EST🌎𓏒𓏒𓏒𓏒
-vous verrez des structures de montagnes ⛰️ s'étendant sur 120 km² ainsi que de nombreuses plaine(...) les conditions de survie dans les montagnes sont ardue a cause de l'absence de vivre ainsi que de point d'eau et autres éléments 🧰 nécessaire à la survie..
+a 19km du point de départ vous verrez des structures de montagnes ⛰️ s'étendant sur 120 km² ainsi que de nombreuses plaine(...) les conditions de survie dans les montagnes sont ardue a cause de l'absence de vivre ainsi que de point d'eau et autres éléments 🧰 nécessaire à la survie..
 
 𓏒𓏒𓏒𓏒OUEST🌎𓏒𓏒𓏒𓏒 
-Vous verrez une vaste ville s'étendant sur 100km² avec des (maisons,marchés,...etc)
+a 20km du point de départ vous verrez une vaste ville s'étendant sur 100km² avec des (maisons,marchés,...etc)
 
 👥 POPULATIONS : 1300hbts
 🪙RICHESSES: la richesse de cette ville est estimé à 100.049.670$💰
