2b6dfbe61ef31d82ca46849258b97dca00bfc15b
LOA <118371674+Gbevon@users.noreply.github.com>
2024-03-17 11:52:18 +0000

    Update level_up.js

--- commandes/level_up.js
+++ commandes/level_up.js
@@ -9,23 +9,23 @@ zokou(
         const { repondre, arg, ms } = commandeOptions;
 
         if (!arg || arg.length === 0)  {
-            const lien = 'https://telegra.ph/file/09174a33bf40d1cf2b3a8.jpg';
+            const lien = 'https://telegra.ph/file/4cbf4618b8ae9a72d55d4.jpg';
             const msg = `➖🛡️ TOUT SAVOIR SUR( le level up⬆️)🛡️
   
 ➖➖➖➖➖➖➖➖➖
-⚠️chat🎧: bienvenue,voici les informations fournies concernant (promotion de niveau)⬇️
+💬chat🎧: bienvenue,voici les informations fournies concernant (promotion de niveau)⬇️
 ➖➖➖➖➖➖➖➖➖
           
-⚠️ pour monter en niveau vous devez accumulé des points d'expérience appeler (XP🎮) au cours de vos aventures et de vos duels 
+✍️ pour monter en niveau vous devez accumulé des points d'expérience appeler (XP🎮) au cours de vos aventures et de vos duels 
           
 ⚙️ pour monter⬆️ du:
 
-⚠️niveau 1➡️niveau2  :il faut 100XP🎮
-⚠️niveau 2➡️ niveau 3:il faut 200XP🎮
-⚠️niveau 3➡️ niveau 4:il faut 225XP🎮
-⚠️niveau 4➡️ niveau 5:il faut 250XP🎮
-⚠️niveau 5➡️ niveau 6:il faut 300XP🎮
-⚠️niveau 6➡️ niveau 7:il faut 600XP🎮
+𖨠niveau 1➡️niveau2  :il faut 100XP🎮
+𖨠niveau 2➡️ niveau 3:il faut 200XP🎮
+𖨠niveau 3➡️ niveau 4:il faut 225XP🎮
+𖨠niveau 4➡️ niveau 5:il faut 250XP🎮
+𖨠niveau 5➡️ niveau 6:il faut 300XP🎮
+𖨠niveau 6➡️ niveau 7:il faut 600XP🎮
 
 ⚙️une fois le nombre d'XP 🎮 requis pour passer au niveau suivant rendez vous à la guilde du jeu pour commencer le processus de mise a niveau.
 ➖➖➖➖➖➖➖➖➖
